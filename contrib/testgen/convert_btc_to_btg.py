4e4904aad1234a0d423d228d08e811f61118bfb0
dannyaw <www222fff@163.com>
2021-12-27 21:18:12 +0800

    Upgrade to v021 for ut issue fix

--- contrib/testgen/convert_btc_to_btg.py
+++ contrib/testgen/convert_btc_to_btg.py
@@ -3,23 +3,30 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
-Generate valid and invalid base58/bech32(m) address and private key test vectors.
+convert btc address to btg address.
 
-Usage:
-    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py valid 70 > ../../src/test/data/key_io_valid.json
-    PYTHONPATH=../../test/functional/test_framework ./gen_key_io_test_vectors.py invalid 70 > ../../src/test/data/key_io_invalid.json
 '''
 # 2012 Wladimir J. van der Laan
 # Released under MIT License
 import os
-from itertools import islice
 from base58 import b58encode_chk, b58decode_chk
-import random
 
 
 if __name__ == '__main__':
 
-    raw = b58decode_chk('3B5fQsEXEaV8v6U3ejYc8XaKXAkyQj2MjV')
-    raw[0] = 'G'
-    b58encode_chk(raw)
-    sys.stdout.write(raw)
+    addr_list = ["3B5fQsEXEaV8v6U3ejYc8XaKXAkyQj2MjV", 
+                 "1KqbBpLy5FARmTPD4VZnDDpYjkUvkr82Pm",
+                 "15CRxFdyRpGZLW9w8HnHvVduizdL5jKNbs",
+                 "11canuhp9X2NocwCq7xNrQYTmUgZAnLK3"]
+
+    for addr in addr_list:
+        print("BTC:", addr)
+        raw = b58decode_chk(addr)
+        raw_array = bytearray(raw)
+        if (raw_array[0] == 5):
+            raw_array[0] = 23
+        elif (raw_array[0] == 0):
+            raw_array[0] = 38
+
+        res = b58encode_chk(raw_array)
+        print("---BTG:", res)
